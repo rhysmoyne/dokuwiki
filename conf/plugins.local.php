20166a1932f7c957a42ff0441654e4c8cd913315
Rhys Moyne <rhys@creativekidssa.com.au>
2016-09-09 20:57:42 +0930

    Add config for wiki.

---
+++ conf/plugins.local.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Local plugin enable/disable settings
+ * Auto-generated through plugin/extension manager
+ *
+ * NOTE: Plugins will not be added to this file unless there is a need to override a default setting. Plugins are
+ *       enabled by default.
+ */
+$plugins['authpgsql'] = 0;
+$plugins['authldap'] = 0;
+$plugins['authad'] = 0;
+$plugins['authmysql'] = 0;
---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admin:$1$ULihq1dZ$n4DM7K3trICU02i9HO25a1:Creative Kids Admin:admin@creativekidssa.com.au:admin,user
